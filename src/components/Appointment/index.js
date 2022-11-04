0c89e9da5b722fcb838325c2e0f04a26c0929a5f
Jeremy Buist <79812985+jbuistjbuist@users.noreply.github.com>
2022-11-04 16:25:26 -0400

    adding the first 'use visual mode' hook

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,9 +3,15 @@ import "./styles.scss"
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import useVisualMode from "hooks/useVisualMode";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
 
 
 export default function Appointment(props) {
+
+  
 
   const {time} = props;
   const {interview} = props || {};
---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react"
+
+
+const useVisualMode = function(initial) {
+
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial])
+
+  const transition = function(newMode, replace = false) {
+    if (replace) {
+      setHistory(prev => {
+        const prevMod = prev.slice(0, -1);
+        return [...prevMod, newMode];
+      })
+    } else {
+      setHistory(prev => [...prev, newMode])
+    }
+
+    setMode(newMode);
+    
+  }
+
+  const back = function() {
+    if (history.length > 1) {
+      setHistory(prev => {
+        setMode(prev[prev.length - 2]);
+        return prev.slice(0, -1);
+      })
+    }
+  }
+
+  
+
+  return { mode, transition, back }
+}
+
+export default useVisualMode;
